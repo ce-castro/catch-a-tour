ba516959169506de22ce700ca92c355f6624faf1
Cesar Castro <ce.castro@outlook.es>
2019-02-11 07:42:01 -0600

    Better seeding

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -10,11 +10,14 @@ class UsersTableSeeder extends Seeder {
      */
     public function run() {
         DB::table('users')->insert([
+            'role_id' => '1',
             'name' => 'Cesar',
             'last_name' => 'Castro',
             'status' => '1',
             'email' => 'ce.castro@outlook.es',
             'password' => bcrypt('cesar'),
+            'created_at' => '2019-02-01 00:00:00',
+            'updated_at' => '2019-02-01 00:00:00',
         ]);
     }
 }
